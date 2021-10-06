205e7ecc43ef49c50e3420f059e077057126846d
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-07 00:43:54 +0200

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-import "./style.css";
+import './style.css';
 
 // create new game
-if (localStorage.getItem("Game-ID") === null) {
+if (localStorage.getItem('Game-ID') === null) {
+  /* eslint-disable */
   async function getID() {
     const response = await fetch(
-      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
       {
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify({
-          name: "Formula 1",
+          name: 'Formula 1',
         }),
         headers: {
-          "Content-type": "application/json; charset=UTF-8",
+          'Content-type': 'application/json; charset=UTF-8',
         },
-      }
+      },
     );
 
     const id = await response.json();
     return id;
   }
-
+  /* eslint-enable */
   getID().then((id) => {
-    localStorage.setItem("Game-ID", JSON.stringify(id));
+    localStorage.setItem('Game-ID', JSON.stringify(id));
   });
 }
 
 async function getScores() {
-  const list = document.querySelector(".score-board");
+  const list = document.querySelector('.score-board');
 
-  list.innerHTML = "";
+  list.innerHTML = '';
 
   const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jM1sByj470X5lUptQV2M/scores"
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jM1sByj470X5lUptQV2M/scores',
   );
   const scores = await response.json();
   return scores;
@@ -39,24 +40,24 @@ async function getScores() {
 
 const refresh = document.querySelector('.refresh');
 refresh.addEventListener('click', () => {
-  getScores().then(scores => {
+  getScores().then((scores) => {
     const user = scores.result;
-    user.forEach(user => {
-      const list = document.querySelector(".score-board");
+    user.forEach((user) => {
+      const list = document.querySelector('.score-board');
       const person = document.createElement('li');
       person.className = 'score';
       person.innerHTML = `${user.user}  :  ${user.score}`;
-      list.appendChild(person)
+      list.appendChild(person);
     });
   });
 });
 
-async function addScore(url = "", data = {}) {
+async function addScore(url = '', data = {}) {
   const response = await fetch(url, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
   });
 
@@ -64,20 +65,20 @@ async function addScore(url = "", data = {}) {
   return scores;
 }
 
-const submitBtn = document.querySelector(".submit");
+const submitBtn = document.querySelector('.submit');
 
-submitBtn.addEventListener("click", (e) => {
+submitBtn.addEventListener('click', (e) => {
   const InputName = document.getElementById('names');
   const InputScore = document.getElementById('scores');
 
-  let name = InputName.value;
-  let score = InputScore.value;
-
+  const name = InputName.value;
+  const score = InputScore.value;
+  /* eslint-disable */ 
   addScore(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jM1sByj470X5lUptQV2M/scores",
-    { user: name, score: score }
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jM1sByj470X5lUptQV2M/scores',
+    { user: name, score: score },
   ).then((data) => { data; });
-
+  /* eslint-enable */
   InputName.value = '';
   InputScore.value = '';
   e.preventDefault();
