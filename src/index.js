55111c7e57cc9c27d822cbc623203577f28a05ee
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-07 00:14:09 +0200

    Create new game, recieve and send data to API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,86 @@
-import './style.css';
+import "./style.css";
 
-const populateUl = () => {
-  const list = document.querySelector('.score-board');
-  list.insertAdjacentHTML('beforeend', `
-                              <li class="score">Name : 100</li>
-                              <li class="score">Name : 20</li>
-                              <li class="score">Name : 50</li>
-                              <li class="score">Name : 78</li>
-                              <li class="score">Name : 125</li>
-                              <li class="score">Name : 77</li>
-                              <li class="score">Name : 42</li>
-                            `);
-};
+// create new game
+if (localStorage.getItem("Game-ID") === null) {
+  async function getID() {
+    const response = await fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          name: "Formula 1",
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
 
-window.onload = populateUl();
+    const id = await response.json();
+    return id;
+  }
+
+  getID().then((id) => {
+    localStorage.setItem("Game-ID", JSON.stringify(id));
+  });
+}
+
+async function getScores() {
+  const list = document.querySelector(".score-board");
+
+  list.innerHTML = "";
+
+  const response = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jM1sByj470X5lUptQV2M/scores"
+  );
+  const scores = await response.json();
+  return scores;
+}
+
+const refresh = document.querySelector('.refresh');
+refresh.addEventListener('click', () => {
+  getScores().then(scores => {
+    const user = scores.result;
+    user.forEach(user => {
+      const list = document.querySelector(".score-board");
+      const person = document.createElement('li');
+      person.className = 'score';
+      person.innerHTML = `${user.user}  :  ${user.score}`;
+      list.appendChild(person)
+    });
+  });
+});
+
+async function addScore(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+
+  const scores = await response.json();
+  return scores;
+}
+
+const submitBtn = document.querySelector(".submit");
+
+submitBtn.addEventListener("click", (e) => {
+  const InputName = document.getElementById('names');
+  const InputScore = document.getElementById('scores');
+
+  let name = InputName.value;
+  let score = InputScore.value;
+
+  addScore(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jM1sByj470X5lUptQV2M/scores",
+    { user: name, score: score }
+  ).then((data) => { data; });
+
+  InputName.value = '';
+  InputScore.value = '';
+  e.preventDefault();
+});
+
+window.onload = getScores();
